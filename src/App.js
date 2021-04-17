d38a379cba56924cdc25090a0fdd6b970204ecbb
yerin <73337811+yerin512@users.noreply.github.com>
2021-04-17 17:18:22 +0900

    Component life cycle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { unstable_concurrentAct } from 'react-dom/test-utils';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
   state = {
     count : 0
   };
@@ -12,7 +16,17 @@ class App extends React.Component{
   minus = () => {
     this.setState((current)=> ({count:current.count - 1}));
   }
+  componentDidMount(){
+    console.log("component rendered")
+  }
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
+  componentWillUnmount(){
+    console.log("Good bye Creul world")
+  }
   render(){
+    console.log("i'm rendering")
     return (
     <div>
       <h1>The numer is: {this.state.count}</h1>
